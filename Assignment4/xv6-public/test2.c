7fafbabac08ce6a3e2f4bc84b79444ff637eb66a
SarbaniB <sarbani.bhattacharyya@hotmail.com>
2020-04-26 23:00:40 -0500

    added test files for part 1 2

---
+++ Assignment4/xv6-public/test2.c
@@ -0,0 +1,68 @@
+#include "types.h"
+#include "user.h"
+#include "fcntl.h"
+#include "stat.h"
+
+int main() {
+  int ext_fd = open("extentfile2.txt", O_CREATE|O_RDWR|O_EXTENT);
+	if(ext_fd < 0) {
+		printf(2, "Cannot open Extent-Based file\n");
+		exit();
+	}
+	printf(1, "Extent Based File Descriptor: %d\n", ext_fd);
+	
+	int norm_fd = open("pointerfile2.txt", O_CREATE|O_RDWR);
+	if(norm_fd < 0) {
+		printf(2, "Cannot open Pointer-Based File\n");
+		exit();
+	}
+	printf(1, "Pointer-Based File Descriptor:: %d\n", norm_fd);
+
+	char buffer[1024]; 
+	for(int i = 0; i < 1024; i++) {
+		buffer[i] = 's';
+	}
+
+	printf(1, "Attempting to write 5KB into extent-based file ... \n\n");
+	
+	for(int i = 0; i < 5; i++) {
+		if(write(ext_fd, &buffer, 1024) != 1024) {
+			printf(2, "Write failed\n");
+			break;
+		}
+	}
+
+	struct stat ext_info;
+	if(fstat(ext_fd, &ext_info) == 0) {
+		printf(1, "Extent-based File Size: %d\n", ext_info.size);
+		int bcount = 0;
+		for(int i = 0; i < 12; i++) {
+			if(!ext_info.extents[i].len) {
+				break;
+			}
+			bcount += ext_info.extents[i].len;
+			printf(1, "Extent [%d] Address: %d, Length: %d\n", i, ext_info.extents[i].addr, ext_info.extents[i].len);
+		}
+		printf(1, "Total Blocks: %d\n", bcount);
+	}
+	printf(1, "Write to extent file successful. \n\n");
+	printf(1, "Attempting to write 5KB into pointer-based file ... \n\n");
+
+	for(int i = 0; i < 5; i++) {
+		if(write(norm_fd, &buffer, 1024) != 1024) {
+			printf(2, "Write failed\n");
+			break;
+		}
+	}
+	
+	struct stat norm_info;
+	if(fstat(norm_fd, &norm_info) == 0) {
+		printf(1, "Pointer-Based File Size: %d\n", norm_info.size);
+	}
+
+	printf(1, "Write to pointer based file successful. \n\n");
+
+	close(ext_fd);
+	close(norm_fd);
+	exit();
+}
